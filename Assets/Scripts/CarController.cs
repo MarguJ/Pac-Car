a1a96c06165242c91412f3e5fdfdae55d5314cc7
MarguJ <margulesjoa@gmail.com>
2025-11-26 13:58:57 -0300

    Arreglar lo de la traccion

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -4,12 +4,26 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
-    public float moveSpeed;
-    
+    public float moveSpeed = 50;
+    public float maxSpeed = 15;
+    public float drag = 0.98f;
+    public float steerAngle = 20;
+    public float traction = 1;
+
     private Vector3 moveForce;
     void Update()
     {
         moveForce += transform.forward * moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime * -1;
         transform.position += moveForce * Time.deltaTime;
+
+        float steerInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up * steerInput * moveForce.magnitude * steerAngle * Time.deltaTime);
+        
+        moveForce *= drag;
+        moveForce = Vector3.ClampMagnitude(moveForce, maxSpeed);
+
+        Debug.DrawRay(transform.position, moveForce.normalized * 3);
+        Debug.DrawRay(transform.position, transform.forward * 3, Color.blue);
+        moveForce = Vector3.Lerp(moveForce.normalized, transform.forward, traction * Time.deltaTime) * moveForce.magnitude;
     }
 }
