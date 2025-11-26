395ffd155c7a3485d412bf1d6cd352a47c1d8b07
MarguJ <margulesjoa@gmail.com>
2025-11-26 09:20:18 -0300

    Started controller

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public float moveSpeed;
+    
+    private Vector3 moveForce;
+    void Update()
+    {
+        moveForce += transform.forward * moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime * -1;
+        transform.position += moveForce * Time.deltaTime;
+    }
+}
