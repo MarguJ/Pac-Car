3b78bcf451049fc4f7988fbf0f2be7ca27530fe2
MarguJ <margulesjoa@gmail.com>
2025-11-29 02:30:48 -0300

    Problem with the terrain

---
+++ Assets/Scripts/EnemyCarScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using UnityEngine.AI;
+using Vector3 = UnityEngine.Vector3;
+
+public class EnemyCarScript : MonoBehaviour
+{
+    [SerializeField] Transform[] patrolPoints;
+    [SerializeField] float pointReachedThreshold = 0.3f;
+    [SerializeField] private float viewDistance = 10;
+    
+    private int currentPointIndex = 0;
+    private NavMeshAgent agent;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+        if (patrolPoints.Length > 0)
+        {
+            agent.SetDestination(patrolPoints[currentPointIndex].position);
+        }
+    }
+    
+    void Update()
+    {
+        RaycastHit hit;
+        Ray enemyView = new Ray(transform.position, transform.position + transform.forward * viewDistance);
+        
+        Debug.DrawLine(transform.position, transform.position + transform.forward * viewDistance, Color.white);
+        
+        if (Physics.Raycast(enemyView, out hit, viewDistance))
+        {
+            if (hit.collider.CompareTag("Player"))
+            {
+                Debug.Log("Player Hit");
+            }
+        }
+        else
+        {
+            if (patrolPoints.Length > 0)
+            {
+                if (!agent.pathPending && agent.remainingDistance <= pointReachedThreshold)
+                {
+                    currentPointIndex = (currentPointIndex + 1) % patrolPoints.Length;
+                    agent.SetDestination(patrolPoints[currentPointIndex].position);
+                }
+            }
+        }
+    }
+}
